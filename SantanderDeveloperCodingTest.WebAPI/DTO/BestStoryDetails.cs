7963c3fc5dc3fd1e4b3ba20331fcc0915fa9ece5
Bogdan V <ketone.svejak@gmail.com>
2023-08-29 02:08:25 +0200

    Implement TimeAsDateTime .

--- SantanderDeveloperCodingTest.WebAPI/DTO/BestStoryDetails.cs
+++ SantanderDeveloperCodingTest.WebAPI/DTO/BestStoryDetails.cs
@@ -2,13 +2,14 @@
 {
     public class BestStoryDetails
     {
-        public string By { get;set; }
+        private static readonly DateTime UnixDateTimeStart = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        public string By { get; set; }
         public int Descendants { get; set; }
-        public int Id {  get; set; }
+        public int Id { get; set; }
         public int[] Kids { get; set; }
         public int Score { get; set; }
         public int Time { get; set; }
-        public DateTime TimeAsDateTime { get; set; }
+        public DateTime TimeAsDateTime => UnixDateTimeStart.AddSeconds(Time);
         public string Title { get; set; }
         public string Type { get; set; }
         public string Url { get; set; }
