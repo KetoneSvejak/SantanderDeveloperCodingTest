d92e38a22f6c327fbecb572ad65fe62fd90be938
Bogdan V <ketone.svejak@gmail.com>
2023-08-29 02:08:25 +0200

    Create HackerNewsHttpClient. Request beststories.json. Call it from BestStoriesController.

--- SantanderDeveloperCodingTest.WebAPI/Controllers/BestStoriesController.cs
+++ SantanderDeveloperCodingTest.WebAPI/Controllers/BestStoriesController.cs
@@ -19,20 +19,23 @@ namespace SantanderDeveloperCodingTest.WebAPI.Controllers
         /// <param name="n">Number of best stories to return.</param>
         /// <returns>An array of the first n "best stories" as returned by the Hacker News API, sorted by their score in a descending order.</returns>
         [HttpGet(Name = "GetBestStories")]
-        public IEnumerable<BestStory> Get(uint n)
+        public async Task<IEnumerable<BestStory>> Get(int n)
         {
-            var response = new BestStory[n];
+            var hackerNewsHttpClient = new HackerNewsHttpClient();
+            var bestStories = await hackerNewsHttpClient.GetBestStories();
+            if (bestStories == null)
+                return new BestStory[0];
             var random = new Random();
-            for (int i = 0; i < n; i++)
-                response[i] = new BestStory
-                {
-                    CommentCount = random.Next(1000),
-                    PostedBy = Path.GetRandomFileName(),
-                    Score = random.Next(1000),
-                    time = DateTime.Now - new TimeSpan(random.Next(1000000)),
-                    Title = Path.GetRandomFileName(),
-                    Uri = "https://" + Path.GetRandomFileName()
-                };
+            var response = bestStories.Take(n).Select(s =>
+             new BestStory
+             {
+                 CommentCount = random.Next(1000),
+                 PostedBy = Path.GetRandomFileName(),
+                 Score = random.Next(1000),
+                 time = DateTime.Now - new TimeSpan(random.Next(1000000)),
+                 Title = Path.GetRandomFileName(),
+                 Uri = "https://" + Path.GetRandomFileName()
+             }).ToArray();
             return response;
         }
     }
---
+++ SantanderDeveloperCodingTest.WebAPI/HackerNewsHttpClient.cs
@@ -0,0 +1,13 @@
+﻿namespace SantanderDeveloperCodingTest.WebAPI
+{
+    public class HackerNewsHttpClient
+    {
+        public async Task<int[]?> GetBestStories()
+        {
+            var httpClient = new HttpClient();
+            var bestStories = await httpClient.GetAsync("https://hacker-news.firebaseio.com/v0/beststories.json");
+            var response = await bestStories.Content.ReadFromJsonAsync<int[]>();
+            return response;
+        }
+    }
+}
